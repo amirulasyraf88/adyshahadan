6321c4dc981185a11b494a8bcf61489ebc36b340
Amirul Asyraf Mohamed Azam <amirul94aiu@gmail.com>
2021-10-04 21:15:42 +0800

    stage 3

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <v-container grid-list-md>
       <v-layout row wrap align-center justify-center>
          
-      <v-avatar size="200">
+      <v-avatar size="150">
       <img
         src="https://cdn.vuetifyjs.com/images/john.jpg"
         alt="John"
@@ -15,19 +15,43 @@
   
       
     </v-layout>
+<br>
     <v-layout>
+    <v-text-card>
+      Hello there,
+    </v-text-card>
+    </v-layout>
+    <v-layout>
+    <v-text-card>
+      If you are feeling connection with our work then we are probably going to be great fit!
+    </v-text-card>
+    </v-layout>
     <br>
-    </v-layout>
      <v-layout row wrap align-center justify-center>
-         
-      
-
- 
+     
    <v-btn block x-large outlined>Link 1<br>hello</v-btn>
-
-  
       
     </v-layout>
+    <br>
+    <v-layout row wrap align-center justify-center>
+     
+   <v-btn block x-large outlined>Link 1<br>hello</v-btn>
+      
+    </v-layout>
+    <br>
+    <v-layout row wrap align-center justify-center>
+     
+   <v-btn block x-large outlined>Link 1<br>hello</v-btn>
+      
+    </v-layout>
+    <v-spacer />
+    <br>
+    <v-layout row wrap align-center justify-center>
+     
+   <v-btn block x-large outlined>Link 1<br>hello</v-btn>
+      
+    </v-layout>
+    
 </v-container>
 
 </template>
