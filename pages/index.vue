983ad44ccf590dbc1a4ab03909a157761c3111dd
Amirul Asyraf Mohamed Azam <amirul94aiu@gmail.com>
2021-10-04 13:37:48 +0800

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,7 @@
 <template>
   <v-container fill-height fluid>
       <v-layout fill-height>
-          <v-row align="center"
-      justify="center">
-      <v-col>sdada</v-col>
-  </v-row>
+          
       <v-avatar>
       <img
         src="https://cdn.vuetifyjs.com/images/john.jpg"
