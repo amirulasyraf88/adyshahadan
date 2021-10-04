b351b8377dfe8d6e2da2590ba52dc1a0a247800b
Amirul Asyraf Mohamed Azam <amirul94aiu@gmail.com>
2021-10-04 13:28:48 +0800

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,17 @@
 <template>
-  <v-container>
+  <v-container fluid>
       <v-avatar>
       <img
         src="https://cdn.vuetifyjs.com/images/john.jpg"
         alt="John"
+        size="80"
       >
     </v-avatar>
+    <v-btn>Link 1</v-btn>
+    <v-btn>Link 1</v-btn>
+    <v-btn>Link 1</v-btn>
+    <v-btn>Link 1</v-btn>
+    <v-btn>Link 1</v-btn>
 </v-container>
 
 </template>
